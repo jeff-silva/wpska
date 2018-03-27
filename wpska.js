99843b93b18421c8a5aa84f2da7ed4df62ba1c4a
Jeferson Inácio <jeferson.i.silva@gmail.com>
2018-03-27 19:34:45 -0300

    Update wpska.js

--- wpska.js
+++ wpska.js
@@ -1,8 +1,8 @@
 window.jQuery || document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"><\/script>');
 
-if (jQuery.fn.modal||false) {
-	document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"><\/script>');
-	document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">');
+if ((jQuery.fn.modal||false)==false) {
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"><\/script>');
+document.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">');
 }
 
 /*! Vue */
